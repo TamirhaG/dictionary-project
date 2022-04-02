3f018ca7888574bc0c58f572a2d95f4282ec7ec4
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2022-04-01 20:09:49 -0500

    Added photos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo-shecodes.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
@@ -19,5 +19,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
